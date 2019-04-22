a2e5d54ed6b95eb7f5a6520b1f321f91e4836c85
William Jacoby <billyjacoby@gmail.com>
2019-04-22 17:44:31 -0400

    initial div styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -19,6 +19,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="full-height one">
+        <div className="item-one" />
+      </div>
+      <div className="full-height two">
+        <div className="item-two" />
+      </div>
     </div>
   );
 }
