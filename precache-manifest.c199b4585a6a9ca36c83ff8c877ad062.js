9fd64f6b49c5d62c5a5481c27ae74cf02ca60ada
William Jacoby <billyjacoby@gmail.com>
2019-04-22 21:28:08 -0400

    Updates

---
+++ precache-manifest.c199b4585a6a9ca36c83ff8c877ad062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d58c1fb1b7d744881f222090bd1917b",
+    "url": "/intersection-observer-hooks/index.html"
+  },
+  {
+    "revision": "685f03fd2fe9d6948314",
+    "url": "/intersection-observer-hooks/static/css/main.63fa199c.chunk.css"
+  },
+  {
+    "revision": "d0a357db47dc1f834cac",
+    "url": "/intersection-observer-hooks/static/js/2.bf4a470d.chunk.js"
+  },
+  {
+    "revision": "685f03fd2fe9d6948314",
+    "url": "/intersection-observer-hooks/static/js/main.1bb109bf.chunk.js"
+  },
+  {
+    "revision": "1c0863ec4b105211e116",
+    "url": "/intersection-observer-hooks/static/js/runtime~main.1348fa20.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/intersection-observer-hooks/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.509a5b87bad6bd9f96dcb3707ab52f69.js"
+  "/intersection-observer-hooks/precache-manifest.c199b4585a6a9ca36c83ff8c877ad062.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/intersection-observer-hooks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
