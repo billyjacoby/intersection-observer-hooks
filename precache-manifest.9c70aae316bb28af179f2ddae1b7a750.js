ae8a27872ee3f6578f71ceb1b2bb3415d76e7fe3
William Jacoby <billyjacoby@gmail.com>
2019-04-22 21:23:15 -0400

    Updates

---
+++ precache-manifest.9c70aae316bb28af179f2ddae1b7a750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed4f2aba1d890f3677a33d38062ec63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86afcd2a065db316bdb5",
+    "url": "/static/css/main.63fa199c.chunk.css"
+  },
+  {
+    "revision": "d0a357db47dc1f834cac",
+    "url": "/static/js/2.bf4a470d.chunk.js"
+  },
+  {
+    "revision": "86afcd2a065db316bdb5",
+    "url": "/static/js/main.0214987f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
